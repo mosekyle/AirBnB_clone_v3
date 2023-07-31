656e174e0a742e582919d789bc8a106209fd37b0
Joseph Otine <joeotine@gmail.com>
2023-07-31 12:26:09 -0700

    update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sunday 30 April
-@author: Moses 
-"""
+"""Init file for views module"""
 from flask import Blueprint
+
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
-if (__name__ == 'api.v1.views'):
-    from api.v1.views.index import *
-    from api.v1.views.states import *
-    from api.v1.views.cities import *
-    from api.v1.views.amenities import *
-    from api.v1.views.users import *
-    from api.v1.views.places import *
-    from api.v1.views.places_reviews import *
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
